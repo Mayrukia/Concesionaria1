7157b3085cb6430d9495c08f08250ea2a1a1b3cb
Mayra Luengo <mayrukia@gmail.com>
2019-06-23 19:40:06 -0300

    Add files via upload

---
+++ src/concesionaria/Vehiculos.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concesionaria;
+
+/**
+ *
+ * @author mayci
+ */
+public interface Vehiculos {
+     public String vehiculoMasCaro(Auto a1, Auto a2, Moto m3, Moto m4);
+     public String vehiculoMasBarato(Auto a1, Auto a2, Moto m3, Moto m4);
+}
